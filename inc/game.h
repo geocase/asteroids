b6af9c34afaa1b2abffc496e118ec47be8d167bc
Casey Georgeson <caseys.georgeson@gmail.com>
2020-03-02 21:28:28 -0800

    Define entity class

--- inc/game.h
+++ inc/game.h
@@ -2,15 +2,28 @@
 #define GAME_H
 
 #include "input.h"
+#include "sector.h"
 
 class cGame;
+class cEntity;
+
+class cEntity {
+public:
+	cPolygon shape;
+	cVertex *nodes;
+	int32_t xPos, yPos;
+};
 
 class cGame {
 public:
+	cEntity player;
+
 	bool running = true;
 
 	void Quit();
 	void Update(cInput *i); 
+
+	cGame();
 };
 
 #endif
