0126fcf2694aec4ca58e04fde216cefd1499a354
Casey Georgeson <caseys.georgeson@gmail.com>
2020-03-03 20:47:22 -0800

    Add misc header

---
+++ inc/misc.h
@@ -0,0 +1,13 @@
+#ifndef MISC_H
+#define MISC_H
+
+class cMiscMath;
+
+class cMiscMath {
+public:
+	double Determination(double a, double b, double c, double d) {
+		return (a * d) - (b * c);
+	};
+}
+
+#endif
